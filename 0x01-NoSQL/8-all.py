1b7909bced2800031ac175244476a598e743c6ac
Ailwei <Ailweimaemu@gmail.com>
2024-05-13 18:59:49 +0000

    8. List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+// MongoDB Operations with Python using pymongo
+
+def list_all(collection):
+    """ List all documents in Python """
+    documents = collection.find()
+
+    if documents.count_documents({}) == 0:
+        return []
+
+    return documents
+
