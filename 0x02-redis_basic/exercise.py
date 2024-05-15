d42501ab6e2feac69c3c6f6c263c4ee48c30ce9e
Ailwei <Ailweimaemu@gmail.com>
2024-05-15 10:55:37 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,35 +1,107 @@
 #!/usr/bin/env python3
 """
-Writing strings to Redis
+store an instance of
+the Redis client as a private variable named _redis (using redis.Redis())
+and flush the instance using flushdb.
 """
 
 import redis
-import uuid
-from typing import Union
+from uuid import uuid4
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """returns a Callable"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper for decorated function"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper for the decorated function"""
+        input_data = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input_data)
+        output_data = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output_data)
+        return output_data
+
+    return wrapper
+
+
+def replay(fn: Callable):
+    """display the history of calls of a particular function"""
+    r = redis.Redis()
+    function_name = fn.__qualname__
+    value = r.get(function_name)
+    try:
+        value = int(value.decode("utf-8"))
+    except Exception:
+        value = 0
+
+    print("{} was called {} times:".format(function_name, value))
+    inputs = r.lrange("{}:inputs".format(function_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(function_name), 0, -1)
+
+    for input_data, output_data in zip(inputs, outputs):
+        try:
+            input_data = input_data.decode("utf-8")
+        except Exception:
+            input_data = ""
+
+        try:
+            output_data = output_data.decode("utf-8")
+        except Exception:
+            output_data = ""
+
+        print("{}(*{}) -> {}".format(function_name, input_data, output_data))
 
 
 class Cache:
+    """Create a Cache class"""
+
     def __init__(self):
-        """
-        store method that takes a data argument and returns a string
-        """
+        """store an instance of the Redis client"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        @functiontools.wrapper(method)
-        def wrapper(self, *args, *kwargs):
-            key = f"{method.__qualname__}:calls"
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-        return wrapper
-
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        store the input data in Redis using the random key and return the key.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        """generate a random key"""
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
