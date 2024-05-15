90557b940e078811e396258838cb387c22a7d362
Ailwei <Ailweimaemu@gmail.com>
2024-05-15 10:07:58 +0000

    . Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,6 +16,16 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        @functiontools.wrapper(method)
+        def wrapper(self, *args, *kwargs):
+            key = f"{method.__qualname__}:calls"
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store the input data in Redis using the random key and return the key.
