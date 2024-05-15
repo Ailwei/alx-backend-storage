5647eff7aa8f23f11d6e2c58492d9166cc96ae40
Ailwei <Ailweimaemu@gmail.com>
2024-05-15 11:29:10 +0000

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python3
+
 """
-A module with tools for request caching and tracking.
+A module for caching and tracking requests.
 """
 
-import requests
 import redis
+import requests
+from functools import wraps
+from typing import Callable
 
+redis_store = redis.Redis()
+"""
+The module-level Redis instance.
+"""
+
+def data_cacher(method: Callable) -> Callable:
+    """
+    Caches the output of fetched data.
+    """
+
+    @wraps(method)
+    def invoker(url) -> str:
+        """
+        The wrapper function for caching the output.
+        """
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+
+    return invoker
+
+@data_cacher
 def get_page(url: str) -> str:
-    # Connect to Redis
-    r = redis.Redis()
-
-    # Track URL access count
-    count_key = f"count:{url}"
-    r.incr(count_key)
-
-    # Check if page content is cached
-    cached_content = r.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
-
-    # Fetch page content from URL
-    response = requests.get(url)
-    page_content = response.text
-
-    # Cache page content with expiration time of 10 seconds
-    r.setex(url, 10, page_content)
-
-    return page_content
+    """
+    Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    """
+    return requests.get(url).text
